2d7149723990516282768debdf82a303744ac594
Tu Nombre <tu@email.com>
2026-04-17 12:26:30 -0400

    Fix Next.js 14 export configuration

--- Kaiser-FrontEnd/next.config.js
+++ Kaiser-FrontEnd/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: false,
+    unoptimized: true,
     domains: ['kaiseringenieria.cl'],
   },
 };
